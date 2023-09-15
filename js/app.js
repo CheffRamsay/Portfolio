d7dc837d4a5cb4f6854da4fad41627e4ddf4f422
lvcas-ca <lucas_.caceres@hotmail.com.ar>
2023-09-15 16:42:01 -0300

    correcion git

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import { animations } from "./animations.js";
+
 import { navMenu } from "./navMenu.js";
 import { preload } from "./preload.js";
 
@@ -7,26 +6,6 @@ const d = document;
 
 d.addEventListener("DOMContentLoaded", e=>{
   preload();
-  animations();
-  navMenu();
-  
-  
-=======
-import { animations } from "./animations.js";
-import { navMenu } from "./navMenu.js";
-<<<<<<< HEAD
-=======
-import { preload } from "./preload.js";
->>>>>>> 9c63fd7 (Last)
+ navMenu();
 
-const d = document;
-
-d.addEventListener("DOMContentLoaded", e=>{
-  navMenu();
-  animations();
-<<<<<<< HEAD
-=======
-  preload();
->>>>>>> 9c63fd7 (Last)
->>>>>>> 0f57479ca5d3c5faa1a1c90fd40e94ebeb832e8a
 });
--- js/preload.js
+++ js/preload.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 const d = document;
@@ -45,36 +44,3 @@ export function preload(){
 
 
 
-=======
-const d = document;
-const w =  window;
-
-export function preload(){
-    const $loader = d.querySelector("#progress");
-    const rectangulo = d.querySelectorAll(".rectangulo-animado");
-    const boxText = d.querySelector(".rectangulo");
-    const boxTextH1 = d.querySelector(".rectangulo h1");
-    const $ab = d.querySelector(".about");
-    const barraNav = d.querySelector(".hambur-container");
-
-    
-    function quitarMarginAbout(){
-      $ab.style.marginTop = "0px";
-    }
-
-    const addAnimationRectangulo = e =>{
-        for (let i = 0; i < rectangulo.length; i++) {
-            rectangulo[i].style.animationName = "ring-1";
-        }
-    }
-
-    w.addEventListener("load", e =>{
-        $loader.parentElement.removeChild($loader);
-        addAnimationRectangulo();
-        boxText.style.animationName = "open-box";
-        boxTextH1.style.animationName = "animacion-texto";
-        setTimeout(quitarMarginAbout, 2000); 
-        barraNav.style.opacity = "1";
-    });
-}
->>>>>>> 0f57479ca5d3c5faa1a1c90fd40e94ebeb832e8a
